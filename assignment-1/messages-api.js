1e3d75a7a501d4a53f1036412fa3afe7a3049a39
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-02-07 23:53:52 +0100

    added request limit middleware

--- assignment-1/messages-api.js
+++ assignment-1/messages-api.js
@@ -3,9 +3,21 @@ const bodyParser = require("body-parser");
 
 const port = 3000;
 const app = express();
+let requestLimit = 0;
 
 const jsonParser = bodyParser.json();
 app.use(jsonParser);
+
+const requestLimitMiddleware = (request, response, next) => {
+  if (requestLimit >= 5) {
+    response.status(429).end();
+  } else {
+    requestLimit += 1;
+    next();
+  }
+};
+
+app.use(requestLimitMiddleware);
 
 app.post("/messages", (request, response, next) => {
   console.log(request.body.text);
