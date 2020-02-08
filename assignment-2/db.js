777475caee1c11c542e96244f43cd30e9cd886c5
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-02-08 13:42:58 +0100

    added CRUD, all functional

--- assignment-2/db.js
+++ assignment-2/db.js
@@ -7,17 +7,17 @@ const databaseUrl =
 const db = new Sequelize(databaseUrl);
 
 db.sync()
-  .then(() =>
-    Promise.all([
-      Movie.create({ title: "Watchmen", yearOfRelease: 2009, synopsis: "---" }),
-      Movie.create({
-        title: "Starship Troopers",
-        yearOfRelease: 1998,
-        synopsis: "---"
-      }),
-      Movie.create({ title: "Alien", yearOfRelease: 1979, synopsis: "---" })
-    ])
-  )
+  //  .then(() =>
+  //    Promise.all([
+  //      Movie.create({ title: "Watchmen", yearOfRelease: 2009, synopsis: "---" }),
+  //      Movie.create({
+  //        title: "Starship Troopers",
+  //        yearOfRelease: 1998,
+  //        synopsis: "---"
+  //      }),
+  //      Movie.create({ title: "Alien", yearOfRelease: 1979, synopsis: "---" })
+  //    ])
+  //  )
   .then(() => console.log("Database updated"))
   .catch(console.error);
 
--- assignment-2/movies/router.js
+++ assignment-2/movies/router.js
@@ -5,6 +5,7 @@ const router = new Router();
 
 //CRUD CREATE
 router.post("/movies", (request, response, next) => {
+  console.log("adding new movie entry");
   const { title, yearOfRelease, synopsis } = request.body;
   Movie.create({
     title: title,
@@ -16,5 +17,35 @@ router.post("/movies", (request, response, next) => {
     })
     .catch(error => next(error));
 });
+
+//CRUD READ
+router.get("/movies", (request, response, next) => {
+  console.log("/movies request received => displaying all entries");
+  Movie.findAll()
+    .then(movie => response.json(movie))
+    .catch(error => next(error));
+});
+
+router.get("/movies/:id", (request, response, next) => {
+  console.log("/movies request received => displaying single entry");
+  Movie.findByPk(request.params.id)
+    .then(movie => response.json(movie))
+    .catch(error => next(error));
+});
+
+//CRUD UPDATE
+router.put("/movies/:id", (request, response, next) =>
+  Movie.findByPk(request.params.id)
+    .then(movie => movie.update(request.body))
+    .then(movie => response.send(movie))
+    .catch(error => next(error))
+);
+
+//CRUD DELETE
+router.delete("/movies/:id", (request, response, next) =>
+  Movie.destroy({ where: { id: request.params.id } })
+    .then(movie => response.send({ movie }))
+    .catch(next)
+);
 
 module.exports = router;
